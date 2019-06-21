db52d1f06fb96e74a4ee28092bd5c503d413aabd
Smbat <smbo.voskanyan2002@gmail.com>
2019-06-21 13:33:30 +0400

    Added main css styles

--- script.js
+++ script.js
@@ -40,10 +40,20 @@
 			for (var i = 0; i < slides.length; i++) {
 				var active = i == 0 ? 'active' : '';
 				elements += "<div class='bo-slide " + active +"'>\n";
-				elements += "<img src='"+ slides[i]['data-url'] + "' alt='" + slides[i]['data-type'] +"' style='width:100%'>\n";
+				elements += "<img src='"+ slides[i]['data-url'] + "' alt='" + slides[i]['data-type'] +"'>\n";
 				elements += "</div>\n";
 			}
-			var list = "<div class='bo-slider' id='slider-" + amount + "'>" + elements + '</div>';
+			elements += "<a class='bo-prev'>&#10094;</a>\n"; // Previous button
+			elements += "<a class='bo-next'>&#10095;</a>\n"; // Next button
+
+			var list = "<div class='bo-slider' id='slider-" + amount + "'>" + elements + '</div>\n';
+			// Navigation dots
+			list += "<div class='bo-dots'>\n";
+			for (var i = 0; i < slides.length; i++) {
+				var active = i == 0 ? 'selected' : '';
+				list += "<span class='bo-dot " + active + "' data-bo-dot-index='"+ i +"'></span>\n"
+			}
+			list += "</div>";
 			return list;
 		});
 		// Creating new Slider object, which will contain main information about our slider
